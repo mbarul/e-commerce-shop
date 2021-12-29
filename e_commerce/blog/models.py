9b5156d0f26a93740866558dfb3e1d7bd222bf0c
mbarul <marek.barul28@gmail.com>
2021-12-29 15:20:11 +0100

    Added templates and views to show our list of posts

--- e_commerce/blog/models.py
+++ e_commerce/blog/models.py
@@ -1,27 +1,43 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
-    PUBLISHED = 'PB'
-    DRAFT = 'DT'
-    STATUS_OF_CHOICES = [(DRAFT,'Draft'),
-    (PUBLISHED,'Published')]
+    PUBLISHED = 'published'
+    DRAFT = 'draft'
+    STATUS_OF_CHOICES = [(DRAFT, 'Draft'),
+                         (PUBLISHED, 'Published')]
     title = models.CharField(max_length=100)
-    slug= models.SlugField(max_length=100, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    slug = models.SlugField(max_length=100, unique_for_date='publish')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now = True)
-    status = models.CharField(max_length=10, choices=STATUS_OF_CHOICES, default=DRAFT)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10, choices=STATUS_OF_CHOICES, default=DRAFT)
+    objects = models.Manager()
+    published = PublishManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_one',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug
+                             ])
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
-        return self.title + ' - ' +str(self.author)
-
-
-
+        return self.title + ' - ' + str(self.author)
--- e_commerce/blog/views.py
+++ e_commerce/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from . models import Post
 
 # Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_one(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',)
+                             #publish_year=year, publish_month=month, publish_day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
